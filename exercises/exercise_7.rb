94a755a916de15042edbf5c2a640c4a5550fb5e4
vagrant <j_khabra11@hotmail.com>
2020-05-26 03:59:49 +0000

    excercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -5,8 +5,21 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
+require_relative '../lib/employee'
+require_relative '../lib/store'
 
 puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "User provide a store name"
+name_gotten = gets.chomp
+
+@new_store = Store.create(name: name_gotten)
+@new_store.save
+
+@new_store.each do |store|
+  puts store
+end
+
+
